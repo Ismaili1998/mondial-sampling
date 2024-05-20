731b6d3b21be623024f41314e0efaed6f680906d
ISMAILI ALAOUI ISMAIL <ismaili.alaoui98@gmail.com>
2024-05-20 23:31:53 +0100

    add search engine 01

--- search_engine/views.py
+++ search_engine/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.db.models import Q
 from order.models import Article
 from project.models import Client, Supplier
-from quoteRequest.models import QuoteRequest
+from quoteRequest.models import QuoteRequest, SupplierCommand
 from commercialOffer.models import CommercialOffer
 from commercialOffer.models import Confirmed_commercialOffer
 from invoice.models import Invoice
@@ -12,17 +12,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 
 def manage_search(request):
+
     keyword = request.GET.get('keyword')
     filter_type = request.GET.get('filter_type') or 'client'
     context = {"keyword":keyword, "filter_type":filter_type}
+
     if keyword:
+
         if filter_type == 'client':
-            context[filter_type]  = get_object_or_404(Client, client_nbr=keyword)
+            context[filter_type] = get_object_or_404(Client, client_nbr=keyword)
             context['commercialOffers'] = CommercialOffer.objects.filter(project__client__client_nbr=keyword)[:50]  
             context['confirmedOffers'] = Confirmed_commercialOffer.objects.filter(project__client__client_nbr=keyword)[:50]  
             context['invoices'] = Invoice.objects.filter(project__client__client_nbr=keyword)[:50]                                         
-        elif filter_type == 'article':
-            context[filter_type]  = get_object_or_404(Article, article_nbr=keyword)
+        
         elif filter_type == 'supplier':
+            context[filter_type]  = get_object_or_404(Supplier, supplier_nbr=keyword)
+            context['supplierCommands'] = SupplierCommand.objects.filter(supplier__supplier_nbr = keyword)[:50]  
+            context['quoteRequests'] = QuoteRequest.objects.filter(supplier__supplier_nbr = keyword)[:50] 
+
+        elif filter_type == 'article':
             context[filter_type] = get_object_or_404(Supplier, supplier_nbr=keyword)
+
     return render(request, f'search_by_{filter_type}.html', context)
